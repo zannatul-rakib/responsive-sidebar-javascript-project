164a15a9b0293174781c46c353bb36cd23fe98d2
zrakib-dev <dev.zrakib@gmail.com>
2023-07-21 12:43:29 +0600

    almost done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("dekhi connect hoice kina");
+const body = document.querySelector("body"),
+  sidebar = body.querySelector(".sidebar"),
+  toggle = body.querySelector(".toggle"),
+  searchBtn = body.querySelector(".search-box"),
+  modeSwitch = body.querySelector(".toggle-switch"),
+  modeText = body.querySelector(".mode-text");
+
+//   theme change
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Light Mode";
+  } else {
+    modeText.innerText = "Dark Mode";
+  }
+});
+
+// close
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
